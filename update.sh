19df2fdcb6cbb15f76685307bf94062ce41d4c41
DriptaSenapati <driptasenapati97@gmail.com>
2022-02-12 14:36:35 +0530

    Scheduler update

--- update.sh
+++ update.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+echo "Starting data update workflow...."
+
+main_branch="master"
+gh_branch="gh-pages"
+
+git config --global user.email "$EMAIL_ID"
+git config --global user.name "$USER_NAME"
+
+
 pip3 install -r requirements.txt
 
 python3 main.py
+
+
+
+git checkout "$gh_branch"
+
+echo "moving updated data to main folder"
+mv -v -f temp/* data/
+rm -rf temp/
+
+git status
+git add .
+git commit -m "datasets updated on - $(date)"
+
+git push origin "$gh_branch"
+
+echo "data update done."
