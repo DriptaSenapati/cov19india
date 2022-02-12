c4448fff50071ba518076f06a6d73c2bb322b522
DriptaSenapati <driptasenapati97@gmail.com>
2022-02-12 13:21:58 +0530

    updater push

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ warnings.filterwarnings("ignore")
 from tqdm import tqdm
 import os
 import json
+import urllib.request
+
+
+def getLastUpdatedDate(url):
+    with urllib.request.urlopen(url) as url:
+        lastData = json.loads(url.read().decode())
+        
+    return lastData["UpdatedDate"]
 
 
 def create(startDate,endDate,obj,dis_obj,curr_path):
@@ -66,56 +74,55 @@ def create(startDate,endDate,obj,dis_obj,curr_path):
 def update(obj,dis_obj,curr_path):
     print("Checking any new date are found or not....")
     data_path = os.path.join(curr_path,"data")
-    if "india_current.json" in os.listdir(curr_path):
-        with open(os.path.join(curr_path,"india_current.json"),"r") as f:
-            curr_data = json.load(f)
+
+    lastDate = getLastUpdatedDate(url = "https://driptasenapati.github.io/cov19india/data/india_current.json")
         
-        save_date = datetime.strptime(curr_data["UpdatedTill"],"%m/%d/%Y")
-        api_date = datetime.strptime(obj.lastDate,"%m/%d/%Y")
-        if api_date > save_date:
-            print(f"New dates are found. Database is updating to {obj.lastDate}...")
+    save_date = datetime.strptime(lastDate,"%m/%d/%Y")
+    api_date = datetime.strptime(obj.lastDate,"%m/%d/%Y")
+    if api_date > save_date:
+        print(f"New dates are found. Database is updating to {obj.lastDate}...")
+        
+        
+        sep_res_json = {
+            "Charts": {
+                    "cum": {k:v.to_dict(orient = "records") for k,v in obj.api_data.items()},
+                    "daily": {k:v.to_dict(orient = "records") for k,v in obj.api_data_daily.items()}
+                },
+            "States": obj.api_data['confirmed'][['STATE/UT','CODE']].to_dict(orient = "records")
             
+        }
+
+        with open(os.path.join(data_path,"states.json"),"w") as f:
+            json.dump(sep_res_json,f)
+        
+        day_diff = (api_date - save_date).days
+        for d in tqdm(range(0,day_diff + 1)):
+            date = datetime.strftime(save_date + timedelta(days=d),"%m/%d/%Y")
+            dashData = obj.getDashboardData(state = "TT",date = date)
+            statewiseData = obj.getStateWiseData(date = date)
+            tableData = obj.getTableDate(date = date)
             
-            sep_res_json = {
-                "Charts": {
-                        "cum": {k:v.to_dict(orient = "records") for k,v in obj.api_data.items()},
-                        "daily": {k:v.to_dict(orient = "records") for k,v in obj.api_data_daily.items()}
-                    },
-                "States": obj.api_data['confirmed'][['STATE/UT','CODE']].to_dict(orient = "records")
-                
-            }
-    
-            with open(os.path.join(data_path,"states.json"),"w") as f:
-                json.dump(sep_res_json,f)
+            dis_wise_data = dis_obj.getDistrictwiseData(date = date)
             
-            day_diff = (api_date - save_date).days
-            for d in tqdm(range(0,day_diff + 1)):
-                date = datetime.strftime(save_date + timedelta(days=d),"%m/%d/%Y")
-                dashData = obj.getDashboardData(state = "TT",date = date)
-                statewiseData = obj.getStateWiseData(date = date)
-                tableData = obj.getTableDate(date = date)
-                
-                dis_wise_data = dis_obj.getDistrictwiseData(date = date)
-                
-                res_json = {
-                        "UpdatedTill":date,
-                        "State": "TT",
-                        "dates": {"firstDate": obj.firstDate,"lastDate": obj.lastDate},
-                        "Dashboard": dashData,
-                        "Table": tableData,
-                        "Map": statewiseData,
-                        "District": dis_wise_data
-                    }
-                curr_file_name = f"india_{(save_date + timedelta(days=d)).strftime('%m-%d-%Y')}.json"
-                if(save_date + timedelta(days=d) == api_date):
-                    curr_file_name = "india_current.json"
-                    with open(os.path.join(curr_path,curr_file_name),"w") as f:
-                        json.dump(res_json,f)
-                
-                with open(os.path.join(data_path,curr_file_name),"w") as f:
+            res_json = {
+                    "UpdatedTill":date,
+                    "State": "TT",
+                    "dates": {"firstDate": obj.firstDate,"lastDate": obj.lastDate},
+                    "Dashboard": dashData,
+                    "Table": tableData,
+                    "Map": statewiseData,
+                    "District": dis_wise_data
+                }
+            curr_file_name = f"india_{(save_date + timedelta(days=d)).strftime('%m-%d-%Y')}.json"
+            if(save_date + timedelta(days=d) == api_date):
+                curr_file_name = "india_current.json"
+                with open(os.path.join(curr_path,curr_file_name),"w") as f:
                     json.dump(res_json,f)
-        else:
-            print("No update available")
+            
+            with open(os.path.join(data_path,curr_file_name),"w") as f:
+                json.dump(res_json,f)
+    else:
+        print("No update available")
     
     
 
